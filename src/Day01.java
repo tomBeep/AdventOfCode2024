1cb96547b997ed4faa10f320fed80a29e4fbf756
Tom Edwards <tom@geneious.com>
2024-12-03 10:57:38 +1300

    Start

---
+++ src/Day01.java
@@ -0,0 +1,26 @@
+public class Day01 {
+
+    public static void main(String[] args) {
+        new Day01().doChallenge();
+    }
+
+    private void doChallenge() {
+        String input = getInput();
+        doPart1(input);
+        doPart2(input);
+    }
+
+    private void doPart1(String input) {
+
+    }
+
+    private void doPart2(String input) {
+
+    }
+
+    private String getInput() {
+        return """
+            123
+            """;
+    }
+}
